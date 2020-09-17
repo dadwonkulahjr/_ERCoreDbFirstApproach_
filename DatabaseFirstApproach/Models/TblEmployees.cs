243512663cc6ad5f4627b3e35c8959feb96bf1d5
Dad S Wonkulah Jr <68390101+dadwonkulahjr@users.noreply.github.com>
2020-09-17 16:04:29 +0000

    Third commit

--- DatabaseFirstApproach/Models/TblEmployees.cs
+++ DatabaseFirstApproach/Models/TblEmployees.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DatabaseFirstApproach.Models
 {
     public partial class TblEmployees
     {
         public int EmployeeId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required, EmailAddress, RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")]
         public string Email { get; set; }
+        [Display(Name ="Gender")]
+        [Required]
         public int? GenderId { get; set; }
+        [Display(Name = "Department")]
+        [Required]
         public int? DepartmentId { get; set; }
+        [Required]
         public decimal? Salary { get; set; }
 
         public virtual TblDepartment Department { get; set; }
